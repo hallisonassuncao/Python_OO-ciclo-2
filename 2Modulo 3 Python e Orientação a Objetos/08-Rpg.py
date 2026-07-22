677daf51b8ff6cb0cf68e60b4f135c5adf3b687b
alunocceia <alunocceia@ifb.local>
2026-07-22 09:26:45 -0300

    Dia 22/07/2026

---
+++ 2Modulo 3 Python e Orientação a Objetos/08-Rpg.py
@@ -0,0 +1,31 @@
+class Personagem:
+    def __init__(self, nome, nivel):
+        self._nome = nome
+        self._nivel = nivel
+
+    def atacar(self):
+        print(f"\n{self._nome} realiza um ataque.")
+
+
+class Guerreiro(Personagem):
+    def __init__(self, nome, nivel, forca):
+        super().__init__(nome, nivel)
+        self.__forca = forca
+
+    def atacar(self):
+        print(f"\n{self._nome} realiza um ataque físico! (Força: {self.__forca}).")
+
+
+def main():
+    personagem = Personagem("Jaspion", 7)
+    guerreiro = Guerreiro("Hulk", 8, 81)
+
+    lista_personagens = [personagem, guerreiro]
+
+    print("\n--- Ataques dos Personagens ---")
+    for p in lista_personagens:
+        p.atacar()
+
+
+if __name__ == "__main__":
+    main()
